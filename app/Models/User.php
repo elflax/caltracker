1420f49b35babcbe33421652b0519ce2ea7c595b
Edgardo Zambrano <=>
2021-10-18 14:16:32 -0400

    Install laravel/ui with bootstrap and auth scaffolding and fix last name typo

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'lastname',
+        'last_name',
         'weight',
         'ideal_weight',
         'height',
