1c04c9a468acd2bfd528ce0a893b608cb770884a
Blas Paredes <blas12.p@gmail.com>
2021-11-04 13:22:59 -0400

    fix meal list for user

--- app/Models/Meal.php
+++ app/Models/Meal.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Meal extends Model
 {
@@ -46,7 +47,9 @@ class Meal extends Model
     }
 
     public static function getByDate($date){
-        return Meal::whereDate('date', \Carbon\Carbon::createFromFormat('m-d-Y', $date)->format('Y-m-d'))->get();
+        return Meal::where('user_id', Auth::id())
+                    ->whereDate('date', \Carbon\Carbon::createFromFormat('m-d-Y', $date)
+                    ->format('Y-m-d'))->get();
     }
 
     public static function del($id){
